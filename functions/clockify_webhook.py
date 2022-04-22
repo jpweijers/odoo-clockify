820385c30fb1f8a64a0fac5b1824b25ba2eff12f
Jean-Paul Weijers <jpweijers@users.noreply.github.com>
2022-04-22 08:07:15 +0200

    added logger class for better logging

--- functions/clockify_webhook.py
+++ functions/clockify_webhook.py
@@ -27,6 +27,29 @@ clockify_session = clockify.ClockifySession(
 )
 
 
+class TimesheetLogger:
+    def __init__(self, project, task, description, duration):
+        self.project = project
+        self.task = task
+        self.description = description
+        self.duration = duration
+
+    def _info(self, msg):
+        return f"{msg} - Timesheet: {self.project} - {self.task} - {self.description} - {self.duration}"
+
+    def _error(self, msg, err):
+        return f"{self.info(msg)} Error: {err}"
+
+    def info(self, msg):
+        logging.info(self._info(msg))
+
+    def error(self, msg, err):
+        logging.info(self.error(msg, err))
+
+
+t_logger: TimesheetLogger
+
+
 def updated(event={}, context={}):
     CLOCKIFY_WEBHOOK_SIGNATURE = os.environ["CLOCKIFY_WEBHOOK_SIGNATURE_UPDATED"]
 
@@ -52,28 +75,35 @@ def manual(event={}, context={}):
 
 
 def process_request(event, signature):
+    global t_logger
+
     logging.info(f"event: {event}")
     if request_is_signed(event, signature):
         clockify_timesheet = get_clockify_timesheets(event)
 
-        odoo_pid, odoo_tid, description, start, end, duration = clockify_timesheet
+        (
+            odoo_pid,
+            odoo_tid,
+            description,
+            start,
+            end,
+            duration,
+        ) = clockify_timesheet
 
         odoo_entry = odoo_session.get_same_day_time_entries(
             odoo_pid, odoo_tid, description, start, end
         )
         duration = odoo.seconds_to_hours(duration)
+        t_logger.duration = duration
 
         if odoo_entry:
             odoo_duration = odoo_entry["unit_amount"]
-            if duration != odoo_duration:
-                if duration:
-                    update_odoo_timesheet(odoo_entry["id"], duration, description)
-                else:
-                    delete_odoo_timesheet(odoo_entry["id"])
+            if duration == 0:
+                delete_odoo_timesheet(odoo_entry["id"])
+            elif duration != odoo_duration:
+                update_odoo_timesheet(odoo_entry["id"], duration, description)
             else:
-                logging.info(
-                    f"Time not changed for: {odoo_pid} - {odoo_tid} - {description} - {duration}"
-                )
+                t_logger.info("Time not changed")
         else:
             create_odoo_timesheet(odoo_pid, odoo_tid, description, duration, start)
 
@@ -83,37 +113,35 @@ def process_request(event, signature):
 
 
 def update_odoo_timesheet(entry_id, duration, description):
+    global t_logger
+
     updated_odoo_entry = odoo_session.update_time_entry(entry_id, duration)
     if "error" not in updated_odoo_entry:
-        logging.info(
-            f"Updated time in odoo for entry ID:{entry_id} - {description} - time: {duration}"
-        )
+        t_logger.info(f"Updated time in Odoo")
     else:
-        logging.error(
-            f"Could not Update time for entry ID:{entry_id} - {description} - time: {duration}"
-        )
+        t_logger.error(f"Could not Update time in Odoo", updated_odoo_entry["error"])
 
 
 def create_odoo_timesheet(odoo_project_id, odoo_task_id, description, duration, start):
+    global t_logger
+
     new_odoo_entry = odoo_session.create_time_entry(
         odoo_project_id, odoo_task_id, description, duration, start
     )
     if "error" not in new_odoo_entry:
-        logging.info(
-            f"Created time in odoo for: {odoo_project_id} - {odoo_task_id} - {description} - {duration}"
-        )
+        t_logger.info("Created time in Odoo")
     else:
-        logging.error(
-            f"Could not Create time for: {odoo_project_id} - {odoo_task_id} - {description} - {duration}. Error: {new_odoo_entry['error']}"
-        )
+        t_logger.error("Could not create time in Odoo", new_odoo_entry["error"])
 
 
 def delete_odoo_timesheet(entry_id):
+    global t_logger
+
     deleted_odoo_entry = odoo_session.unlink_time_entry(entry_id)
     if "error" not in deleted_odoo_entry:
-        logging.info(f"Deleted time entry with ID: {entry_id}")
+        t_logger.info("Deleted time entry from Odoo")
     else:
-        logging.error(f"Could not delete time entry with ID: {entry_id}")
+        t_logger.info("Could not delete time entry from Odoo")
 
 
 def request_is_signed(event, signature):
@@ -125,6 +153,8 @@ def request_is_signed(event, signature):
 
 
 def get_clockify_timesheets(event):
+    global t_logger
+
     body = json.loads(event["body"])
     description = body.get("description")
     project = body.get("project")
@@ -133,6 +163,7 @@ def get_clockify_timesheets(event):
 
         odoo_pid = clockify.odoo_id_from_note(project.get("note"))
         odoo_tid = clockify.odoo_id_from_task(task.get("name"))
+
         clockify_task_id = task.get("id")
 
         start = date.fromisoformat(body["timeInterval"]["start"][:10])
@@ -150,7 +181,16 @@ def get_clockify_timesheets(event):
             .get(description, 0)
         )
 
-        return (odoo_pid, odoo_tid, description, start, end, duration)
+        t_logger = TimesheetLogger(project["name"], task["name"], description, duration)
+
+        return (
+            odoo_pid,
+            odoo_tid,
+            description,
+            start,
+            end,
+            duration,
+        )
 
 
 if __name__ == "__main__":
